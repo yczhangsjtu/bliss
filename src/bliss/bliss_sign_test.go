00ee354c98311b442e6a866867eb5149e0ffaf9d
Yuncong Zhang <shjdzhangyuncong@sjtu.edu.cn>
2017-11-22 09:44:52 +0800

    Implement SampleExpCt and SampleGaussCt, and benchmark the gaussian

--- src/bliss/bliss_sign_test.go
+++ src/bliss/bliss_sign_test.go
@@ -3,6 +3,7 @@ package bliss
 import (
 	_ "fmt"
 	_ "io/ioutil"
+	"params"
 	"sampler"
 	_ "strconv"
 	_ "strings"
@@ -55,4 +56,88 @@ func TestSignVerify(t *testing.T) {
 			t.Errorf("Failed to verify signature for version %d: %s", i, err.Error())
 		}
 	}
+}
+
+func benchSign(b *testing.B, version int) {
+	seed := make([]uint8, sampler.SHA_512_DIGEST_LENGTH)
+	for i := 0; i < len(seed); i++ {
+		seed[i] = uint8(i % 8)
+	}
+	entropy, err := sampler.NewEntropy(seed)
+	if err != nil {
+		b.Errorf("Error in initializing entropy: %s", err.Error())
+	}
+
+	key, err := GeneratePrivateKey(version, entropy)
+	if err != nil {
+		b.Errorf("Error in generating private key: %s", err.Error())
+	}
+
+	msg := []byte("Hello world")
+	for i := 0; i < b.N; i++ {
+		key.Sign(msg, entropy)
+	}
+}
+
+func BenchmarkSignBliss0(b *testing.B) {
+	benchSign(b, params.BLISS_B_0)
+}
+
+func BenchmarkSignBliss1(b *testing.B) {
+	benchSign(b, params.BLISS_B_1)
+}
+
+func BenchmarkSignBliss2(b *testing.B) {
+	benchSign(b, params.BLISS_B_2)
+}
+
+func BenchmarkSignBliss3(b *testing.B) {
+	benchSign(b, params.BLISS_B_3)
+}
+
+func BenchmarkSignBliss4(b *testing.B) {
+	benchSign(b, params.BLISS_B_4)
+}
+
+func benchVerify(b *testing.B, version int) {
+	seed := make([]uint8, sampler.SHA_512_DIGEST_LENGTH)
+	for i := 0; i < len(seed); i++ {
+		seed[i] = uint8(i % 8)
+	}
+	entropy, err := sampler.NewEntropy(seed)
+	if err != nil {
+		b.Errorf("Error in initializing entropy: %s", err.Error())
+	}
+
+	key, err := GeneratePrivateKey(version, entropy)
+	if err != nil {
+		b.Errorf("Error in generating private key: %s", err.Error())
+	}
+
+	pub := key.PublicKey()
+	msg := []byte("Hello world")
+	sig, err := key.Sign(msg, entropy)
+	for i := 0; i < b.N; i++ {
+		pub.Verify(msg, sig)
+	}
+}
+
+func BenchmarkVerifyBliss0(b *testing.B) {
+	benchVerify(b, params.BLISS_B_0)
+}
+
+func BenchmarkVerifyBliss1(b *testing.B) {
+	benchVerify(b, params.BLISS_B_1)
+}
+
+func BenchmarkVerifyBliss2(b *testing.B) {
+	benchVerify(b, params.BLISS_B_2)
+}
+
+func BenchmarkVerifyBliss3(b *testing.B) {
+	benchVerify(b, params.BLISS_B_3)
+}
+
+func BenchmarkVerifyBliss4(b *testing.B) {
+	benchVerify(b, params.BLISS_B_4)
 }
--- src/sampler/sampler.go
+++ src/sampler/sampler.go
@@ -5,7 +5,6 @@ import (
 	"params"
 )
 
-
 type Sampler struct {
 	sigma      uint32
 	ell        uint32
@@ -21,32 +20,32 @@ type Sampler struct {
 }
 
 func invalidSampler() *Sampler {
-	return &Sampler{0,0,0,0,0,0,[]uint8{},[]uint8{},nil}
+	return &Sampler{0, 0, 0, 0, 0, 0, []uint8{}, []uint8{}, nil}
 }
 
-func NewSampler(sigma,ell,prec uint32, entropy *Entropy) (*Sampler, error) {
-	columns := prec/8
-	ctable,err := getTable(sigma,ell,prec)
+func NewSampler(sigma, ell, prec uint32, entropy *Entropy) (*Sampler, error) {
+	columns := prec / 8
+	ctable, err := getTable(sigma, ell, prec)
 	if err != nil {
-		return invalidSampler(),err
+		return invalidSampler(), err
 	}
-	ksigma := getKSigma(sigma,prec)
+	ksigma := getKSigma(sigma, prec)
 	if ksigma == 0 {
-		return invalidSampler(),fmt.Errorf("Failed to get kSigma")
+		return invalidSampler(), fmt.Errorf("Failed to get kSigma")
 	}
-	ksigmabits := getKSigmaBits(sigma,prec)
+	ksigmabits := getKSigmaBits(sigma, prec)
 	if ksigmabits == 0 {
-		return invalidSampler(),fmt.Errorf("Failed to get kSigmaBits")
+		return invalidSampler(), fmt.Errorf("Failed to get kSigmaBits")
 	}
-	return &Sampler{sigma,ell,prec,columns,ksigma,ksigmabits,ctable,[]uint8{},entropy},nil
+	return &Sampler{sigma, ell, prec, columns, ksigma, ksigmabits, ctable, []uint8{}, entropy}, nil
 }
 
 func New(version int, entropy *Entropy) (*Sampler, error) {
 	param := params.GetParam(version)
 	if param == nil {
-		return nil,fmt.Errorf("Failed to get parameter")
+		return nil, fmt.Errorf("Failed to get parameter")
 	}
-	return NewSampler(param.Sigma,param.Ell,param.Prec,entropy)
+	return NewSampler(param.Sigma, param.Ell, param.Prec, entropy)
 }
 
 // Sample Bernoulli distribution with probability p
@@ -54,7 +53,7 @@ func New(version int, entropy *Entropy) (*Sampler, error) {
 // the real probability is p/2^d, where d is the number of
 // bits of p
 func (sampler *Sampler) sampleBer(p []uint8) bool {
-	for _,pi := range p {
+	for _, pi := range p {
 		uc := sampler.random.Char()
 		if uc < pi {
 			return true
@@ -72,8 +71,8 @@ func (sampler *Sampler) SampleBerExp(x uint32) bool {
 	mask := uint32(1) << ri
 	start := ri * sampler.columns
 	for mask > 0 {
-		if x & mask != 0 {
-			if !sampler.sampleBer(sampler.ctable[start:start+sampler.columns]) {
+		if x&mask != 0 {
+			if !sampler.sampleBer(sampler.ctable[start : start+sampler.columns]) {
 				return false
 			}
 		}
@@ -81,6 +80,26 @@ func (sampler *Sampler) SampleBerExp(x uint32) bool {
 		start -= sampler.columns
 	}
 	return true
+}
+
+func (sampler *Sampler) SampleBerExpCt(x uint32) bool {
+	var xi, i, ret, start, bit uint32
+	start = 0
+	ret = 1
+
+	xi = x
+	for i = sampler.ell - 1; i != 0; i-- {
+		if sampler.sampleBer(sampler.ctable[start : start+sampler.columns]) {
+			bit = 1
+		} else {
+			bit = 0
+		}
+		ret = ret * (1 - (xi & 1) + uint32(bit)*(xi&1))
+		xi >>= 1
+		start += sampler.columns
+	}
+
+	return ret != 0
 }
 
 // Sample Bernoulli distribution with probability p = 1/cosh(-x/(2*sigma^2))
@@ -127,7 +146,7 @@ restart:
 // Sample according to Discrete Gauss Distribution
 // exp(-x^2/(2*sigma*sigma))
 func (sampler *Sampler) SampleGauss() int32 {
-	var x,y uint32
+	var x, y uint32
 	var u bool
 	for {
 		x = sampler.SampleBinaryGauss()
@@ -137,16 +156,46 @@ func (sampler *Sampler) SampleGauss() int32 {
 				break
 			}
 		}
-		e := y * (y + 2 * uint32(sampler.kSigma) * x)
+		e := y * (y + 2*uint32(sampler.kSigma)*x)
 		u = sampler.random.Bit()
-		if (x | y) != 0 || u {
+		if (x|y) != 0 || u {
 			if sampler.SampleBerExp(e) {
 				break
 			}
 		}
 	}
 
-	valPos := int32(uint32(sampler.kSigma) * x + y)
+	valPos := int32(uint32(sampler.kSigma)*x + y)
+	if u {
+		return valPos
+	} else {
+		return -valPos
+	}
+}
+
+// Sample according to Discrete Gauss Distribution, constant time
+// exp(-x^2/(2*sigma*sigma))
+func (sampler *Sampler) SampleGaussCt() int32 {
+	var x, y uint32
+	var u bool
+	for {
+		x = sampler.SampleBinaryGauss()
+		for {
+			y = sampler.random.Bits(int(sampler.kSigmaBits))
+			if y < uint32(sampler.kSigma) {
+				break
+			}
+		}
+		e := y * (y + 2*uint32(sampler.kSigma)*x)
+		u = sampler.random.Bit()
+		if (x|y) != 0 || u {
+			if sampler.SampleBerExpCt(e) {
+				break
+			}
+		}
+	}
+
+	valPos := int32(uint32(sampler.kSigma)*x + y)
 	if u {
 		return valPos
 	} else {
