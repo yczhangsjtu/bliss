a9ebca8aef54e4fbbd6cf0406b3e620884a26346
Yuncong Zhang <shjdzhangyuncong@sjtu.edu.cn>
2017-12-08 13:50:21 +0800

    Document bit_packer.go.

--- src/huffman/bit_packer.go
+++ src/huffman/bit_packer.go
@@ -1,3 +1,4 @@
+// Package huffman implements the huffman encoding and decoding scheme.
 package huffman
 
 import (
@@ -5,15 +6,25 @@ import (
 )
 
 const (
+	// Maximum number of BYTES that can be packed in the bit packer.
+	// The number of bits is 8 times this.
 	MAX_PACKER_SIZE int = 8192
 )
 
+// The BitPacker class implements a bit packer, which starts up empty, and
+// stores arbitrary number of bits into a byte array.
+// It keeps track of the number of bits stored, and can at any time exposes the
+// slice of bytes that is storing the bits, which can input into and be
+// unpacked by a bit unpacker.
 type BitPacker struct {
 	data  [MAX_PACKER_SIZE]byte
 	nbyte uint32
 	nbit  uint32
 }
 
+// The BitUnpacker class unpacks a byte array into arbitrary size consecutive
+// bits. It starts up with a full byte array, and the bit pointer pointing to
+// position zero.
 type BitUnpacker struct {
 	data  []byte
 	pbyte uint32
@@ -21,12 +32,16 @@ type BitUnpacker struct {
 	size  uint32
 }
 
+// Create a new bit packer.
 func NewBitPacker() *BitPacker {
 	return &BitPacker{
 		[MAX_PACKER_SIZE]byte{}, 0, 0,
 	}
 }
 
+// Create a new bit unpacker to unpack the given byte array. Since the length
+// of the byte array does not contain enough information for the number of
+// bits, an extra number is passed to specify the total number of bits.
 func NewBitUnpacker(data []byte, nbit uint32) *BitUnpacker {
 	if int(nbit) > len(data)*8 {
 		return nil
@@ -36,6 +51,10 @@ func NewBitUnpacker(data []byte, nbit uint32) *BitUnpacker {
 	}
 }
 
+// Write bits into a bit packer. The given integer are stored in big endian,
+// i.e. the higher bits are stored in earlier bytes.
+// For the last byte, if it is not full, the bits are stored in higher part of
+// this byte.
 func (packer *BitPacker) WriteBits(code uint64, nbit uint32) error {
 	if int(packer.nbyte) >= MAX_PACKER_SIZE {
 		return fmt.Errorf("Packer full!")
@@ -46,12 +65,10 @@ func (packer *BitPacker) WriteBits(code uint64, nbit uint32) error {
 		}
 		left := 8 - packer.nbit
 		if nbit < left {
-			// packer.data[packer.nbyte] <<= nbit
 			packer.data[packer.nbyte] |= byte(code&uint64(uint32(1<<nbit)-1)) << (left - nbit)
 			packer.nbit += nbit
 			return nil
 		} else {
-			// packer.data[packer.nbyte] <<= left
 			packer.data[packer.nbyte] |= byte((code >> (nbit - left)) & uint64(uint32(1<<left)-1))
 			packer.nbit = 0
 			packer.nbyte += 1
@@ -62,10 +79,13 @@ func (packer *BitPacker) WriteBits(code uint64, nbit uint32) error {
 	return nil
 }
 
+// Returns the number of bits that is left in the bit-unpacker.
 func (unpacker *BitUnpacker) Left() uint32 {
 	return unpacker.size - (unpacker.pbyte*8 + unpacker.pbit)
 }
 
+// Read a specific number of bits in the bit unpacker, and store the result
+// in the lower bits of an uint64.
 func (unpacker *BitUnpacker) ReadBits(nbit uint32) (uint64, error) {
 	if unpacker.Left() < nbit {
 		return 0, fmt.Errorf("Not enough bits left!")
@@ -89,10 +109,12 @@ func (unpacker *BitUnpacker) ReadBits(nbit uint32) (uint64, error) {
 	return ret, nil
 }
 
+// Returns the number of bits stored in the bit packer.
 func (packer *BitPacker) Size() uint32 {
 	return packer.nbyte*8 + packer.nbit
 }
 
+// Returns the slice of bytes containing the stored bits.
 func (packer *BitPacker) Data() []byte {
 	return packer.data[:(packer.Size()+7)/8]
 }
